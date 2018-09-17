88993be46b1f293d96a53b0f2c2b6d1d68a19232
dennisderoover <dennis.de.roover1@gmail.com>
2018-09-17 23:27:59 +0200

    Komen eten

--- js/sms.js
+++ js/sms.js
@@ -6,12 +6,14 @@ const client = require('twilio')(accountSid, authToken);
 
 var numbersToMessage = ["+32485836453", "+32485755249"]
 
-numbersToMessage.forEach(function(number){
-  var message = client.messages.create({
-    body: 'Eten staat klaar!',
-    from: '+32460201164',
-    to: number
-  })
-  .then(message =>  console.log(message.status))
-  .done();
-});
+function sendSms() {
+    numbersToMessage.forEach(function(number){
+        var message = client.messages.create({
+            body: 'Eten staat klaar!',
+            from: '+32460201164',
+            to: number
+        })
+        .then(message =>  console.log(message.status))
+        .done();
+    });
+}
